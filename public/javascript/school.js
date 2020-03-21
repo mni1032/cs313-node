b6c4e66e43ec0d81c65a2f61ec469bc1837f5d9f
Spencer Rhoades <spencerrhoades@Spencers-Air.home>
2020-03-21 10:07:31 -0600

    add chapter select list to main page

--- public/javascript/school.js
+++ public/javascript/school.js
@@ -1,10 +1,11 @@
 function fillChapters() {
     var bookSelect = document.getElementById("book");
     var selectedBook = bookSelect.options[bookSelect.selectedIndex].value;
+    book = encodeURIComponent(selectedBook);
     $('#chapter').removeAttr("disabled");
     $.ajax({
         type: "GET",
-        url: "/chapters?book=" + selectedBook,
+        url: "/chapters?book=" + book,
         success: function(response){
             chapters = JSON.parse(response);
             $("#chapter").append(chapters.chapters);
