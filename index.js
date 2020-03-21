a5620f512f4912ec294c17905d97fc3dd8e77ddc
Spencer Rhoades <spencerrhoades@Spencers-Air.home>
2020-03-21 12:40:45 -0600

    add chapter select list to main page

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var postCalc = require('./postCalc.js');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
