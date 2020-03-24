6f3ac2b51abec4db8711786e6075f3595fd55838
Spencer Rhoades <spencerrhoades@Spencers-Air.home>
2020-03-24 16:32:46 -0600

    team activity

--- index.js
+++ index.js
@@ -34,26 +34,75 @@ app.get('/postCalcForm', function(req, res) {
 app.get('/postCalcRate', postCalc.calcRate);
 
 /* Team 12 */
-// app.use(session({???}));
+var logRequest = function (req, res, next) {
+  console.log("Received a request for: " + req.url);
+  next();
+}
+app.use(logRequest);
+
+
+var verifyLogin = function (req, res, next) {
+  if (req.session.username) {
+    next();
+  } else {
+    res.writeHead(401, {"Content-Type": "application/json"});
+    res.write(JSON.stringify({status: 401, success: false}));
+    res.end();
+  }  
+}
+app.use(session({
+  secret: 'much secret',
+  resave: false,
+  saveUninitialized: true
+}));
+
+function connectToDb() {
+  const connectionString = "postgres://mohnqqmhlyxftp:3656ef63652e5d369a0bcc7391b883a8deeee9a2e9bb32a63174683c6b08f626@ec2-23-22-156-110.compute-1.amazonaws.com:5432/d5ps07nkfvc3dm?ssl=true";
+  const pool = new Pool({connectionString: connectionString});
+  return pool;
+}
+
 app.post('/login', function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
 
-  var json = {success: false};
-  if (username == 'admin' && password == 'password') {
-    json.success = true; 
-  }
-  res.json(json);
+  var id = null;
+  var pool = connectToDb();
+  var params = [username, password];
+  var sql = 'SELECT id FROM member WHERE username = $1 AND password = $2';
+  pool.query(sql, params, function(err, result) {
+    if (err) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write("ERROR IN QUERY");
+        res.end();
+    }
+
+    id = result.rows[0].id;
+    var json = {success: false};
+    if (id != null) {
+      json.success = true; 
+      req.session.username = username;
+      req.session.password = password;
+    }
+    res.json(json);
+  });  
 });
 
 app.post('/logout', function(req, res) {
-  if (req.session) {
-    
+  var json = {success: false};
+  if (req.session.username) {
+    req.session.destroy();
+    json.success = true; 
   }
+  res.json(json);
 
 });
+
+app.use(verifyLogin);
 app.get('/getServerTime', function(req, res) {
-  res.send(Date.now());
+  var time = new Date();
+  var result = {success: true, time: time};
+  res.json(result);
 });
 
 /* End Team 12 */
